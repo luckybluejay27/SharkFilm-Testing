ed26f072c018a1bddd7c14d12be4422444da1081
luckybluejay27 <luckybluejay27@gmail.com>
2024-09-20 05:04:12 -0700

    Update HomeNav.vue

--- src/components/HomeNav.vue
+++ src/components/HomeNav.vue
@@ -1,11 +1,11 @@
 <template>
   <ul class="HomeNav">
-    <li><RouterLink :to="{ name: 'Comms' }">Comms Page</RouterLink></li>
-    <li><RouterLink :to="{ name: 'Home' }">Home View</RouterLink></li>
+    <li><RouterLink :to="{ name: 'CommsPage' }">CommsPage</RouterLink></li>
+    <li><RouterLink :to="{ name: 'HomeView' }">Home View</RouterLink></li>
     <li><RouterLink :to="{ name: 'Library' }">Library</RouterLink></li>
     <li><RouterLink :to="{ name: 'Login' }">Login</RouterLink></li>
     <li><RouterLink :to="{ name: 'Portfolio' }">Portfolio</RouterLink></li>
-    <li><RouterLink :to="{ name: 'Support' }">Support View</RouterLink></li>
+    <li><RouterLink :to="{ name: 'SupportView' }">Support View</RouterLink></li>
   </ul>
 </template>
 
@@ -19,12 +19,20 @@ import Portfolio from "@/views/Portfolio.vue";
 import SupportView from "@/views/SupportView.vue";
 
 const routes = [
-  { path: "/", name: "Home", component: HomeView },
-  { path: "/comms", name: "Comms", component: CommsPage },
-  { path: "/library", name: "Library", component: Library },
-  { path: "/login", name: "Login", component: Login },
-  { path: "/portfolio", name: "Portfolio", component: Portfolio },
-  { path: "/support", name: "Support", component: SupportView },
+  { path: "/src/views/HomeView.vue", name: "HomeView", component: HomeView },
+  { path: "/src/views/CommsPage.vue", name: "CommsPage", component: CommsPage },
+  { path: "/src/views/Library.vue", name: "Library", component: Library },
+  { path: "/src/views/Login.vue", name: "Login", component: Login },
+  {
+    path: "src/views/Portfolio.vue",
+    name: "Portfolio",
+    component: Portfolio,
+  },
+  {
+    path: "src/views/SupportView.vue",
+    name: "SupportView",
+    component: SupportView,
+  },
 ];
 
 const router = createRouter({
