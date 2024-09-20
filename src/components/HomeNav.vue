9b467d9bdecf933e08d46b99158290612e86eb46
luckybluejay27 <luckybluejay27@gmail.com>
2024-09-20 05:28:08 -0700

    Origen Reset
    Back to working styles, without working links. Will be adding in router after study

--- src/components/HomeNav.vue
+++ src/components/HomeNav.vue
@@ -1,46 +1,18 @@
 <template>
   <ul class="HomeNav">
-    <li><RouterLink :to="{ name: 'CommsPage' }">CommsPage</RouterLink></li>
-    <li><RouterLink :to="{ name: 'HomeView' }">Home View</RouterLink></li>
-    <li><RouterLink :to="{ name: 'Library' }">Library</RouterLink></li>
-    <li><RouterLink :to="{ name: 'Login' }">Login</RouterLink></li>
-    <li><RouterLink :to="{ name: 'Portfolio' }">Portfolio</RouterLink></li>
-    <li><RouterLink :to="{ name: 'SupportView' }">Support View</RouterLink></li>
+    <li><a href="#/HomeView">Home</a></li>
+    <li><a href="#/CommsPage">Commissions</a></li>
+    <li><a href="#/Library">Library</a></li>
+    <li><a href="#/Login">Login</a></li>
+    <li><a href="#/Portfolio">Portfolio</a></li>
+    <li><a href="#/SupportView">Support</a></li>
   </ul>
 </template>
 
 <script>
-import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
-import CommsPage from "@/views/CommsPage.vue";
-import Library from "@/views/Library.vue";
-import Login from "@/views/Login.vue";
-import Portfolio from "@/views/Portfolio.vue";
-import SupportView from "@/views/SupportView.vue";
-
-const routes = [
-  { path: "/src/views/HomeView.vue", name: "HomeView", component: HomeView },
-  { path: "/src/views/CommsPage.vue", name: "CommsPage", component: CommsPage },
-  { path: "/src/views/Library.vue", name: "Library", component: Library },
-  { path: "/src/views/Login.vue", name: "Login", component: Login },
-  {
-    path: "src/views/Portfolio.vue",
-    name: "Portfolio",
-    component: Portfolio,
-  },
-  {
-    path: "src/views/SupportView.vue",
-    name: "SupportView",
-    component: SupportView,
-  },
-];
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-});
-
-export default router;
+export default {
+  name: "HomeNav",
+};
 </script>
 
 <style>
