6ac2a5d1be0534b02a963e6a0ccc1b4046ae858d
luckybluejay27 <luckybluejay27@gmail.com>
2024-10-05 04:49:06 -0700

    Responsiveness (Mobile later ffs)

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,22 +1,57 @@
 <template>
   <div class="page">
     <SharkLogo class="SharkSplash" />
-    <h1>Scrolling</h1>
+    <h2 :class="{ 'animate-text': isAnimating }">{{ animationText }}</h2>
+    <h1>Promo Content Here</h1>
   </div>
 </template>
 
 <script>
   export default {
     name: 'Home',
+    data () {
+      return {
+        isAnimating: false,
+        animationText: 'Animation: Unhinged',
+        scrollPosition: 0,
+      }
+    },
+    mounted () {
+      window.addEventListener('scroll', this.handleScroll)
+    },
+    beforeUnmount () {
+      window.removeEventListener('scroll', this.handleScroll)
+    },
+    methods: {
+      handleScroll () {
+        const scrollPosition = window.scrollY
+        if (scrollPosition > 100 && !this.isAnimating) {
+          this.isAnimating = true
+          this.animationText = 'Animation: Untethered'
+        } else if (scrollPosition < 100 && this.isAnimating) {
+          this.isAnimating = false
+          this.animationText = 'Animation: Unhinged'
+        }
+      },
+    },
   }
 </script>
 
 <style>
-  h1 {
-    margin-top: 100%;
-  }
   .SharkSplash {
     position: absolute;
     transform-origin: center;
+  }
+  h1 {
+    margin-top: 100vh;
+  }
+  h2 {
+    font-size: 3vmin;
+    opacity: 0.8;
+    font-family: 'ConcertOne';
+    transform: translate(76vmin, 55vmin);
+  }
+  .animate-text {
+    transition: all 0.5s;
   }
 </style>
