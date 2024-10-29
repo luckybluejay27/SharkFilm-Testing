8371681b334143c09b461c29eb4540fb656ac573
luckybluejay27 <luckybluejay27@gmail.com>
2024-10-29 03:29:13 -0700

    Updated Promo Mat

--- src/components/CarouselPromo.vue
+++ src/components/CarouselPromo.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="CarouselPromo">
-    <h1> CarouselPromo</h1>
+    <h1> Our Works </h1>
+    <div class="carousel-item">
+      <img alt="Carcinogen-Game" src="@/assets/Carcinogen.jpg">
+      <p>Carcinogen</p>
+    </div>
+    <div class="carousel-item">
+      <img alt="Coffee&Magic-Anime" src="@/assets/CNM.jpg">
+      <p>Coffee & Magic</p>
+    </div>
   </div>
 </template>
 
   <script>
+  export default {
+    name: 'Carousel',
+  }
   </script>
