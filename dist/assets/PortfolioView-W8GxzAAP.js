5d97102e9363a06eea387058135dab3997901995
luckybluejay27 <luckybluejay27@gmail.com>
2024-11-16 01:31:23 -0800

    Fixed ScrollBars

---
+++ dist/assets/PortfolioView-W8GxzAAP.js
@@ -0,0 +1,1 @@
+import{_ as n,a as t}from"./PrimeNav-wNhS4z7x.js";import{_ as c,o as r,c as _,g as e,a as p,F as i}from"./index-B4l6VuTz.js";const l={name:"PortfolioView",components:n,Socials:t};function m(f,o,u,d,g,P){const s=n,a=t;return r(),_(i,null,[e(s),e(a),o[0]||(o[0]=p("div",{class:"page"},"Portfolio",-1))],64)}const x=c(l,[["render",m]]);export{x as default};
