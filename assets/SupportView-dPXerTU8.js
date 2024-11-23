671d5f79fb7cbdc65eb9f02eb1d40b47f0ea0f76
luckybluejay27 <luckybluejay27@gmail.com>
2024-11-23 03:48:40 -0800

    Updates

---
+++ assets/SupportView-dPXerTU8.js
@@ -0,0 +1,1 @@
+import{_ as n,a as t}from"./PrimeNav-B86GsKfM.js";import{_ as p,o as c,c as r,g as e,a as _,F as m}from"./index-B3CzXvhw.js";const i={name:"SupportView",components:n,Socials:t};function u(l,o,d,f,g,S){const s=n,a=t;return c(),r(m,null,[e(s),e(a),o[0]||(o[0]=_("div",{class:"page"},"Support",-1))],64)}const x=p(i,[["render",u]]);export{x as default};
