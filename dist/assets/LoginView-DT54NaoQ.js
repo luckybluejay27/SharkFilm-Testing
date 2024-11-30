0bcbbd9a59e1f0fc2b08aca3183148af204c30d9
luckybluejay27 <luckybluejay27@gmail.com>
2024-11-30 03:08:02 -0800

    Rando Build Commit

--- dist/assets/LoginView-DT54NaoQ.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,a as s}from"./MainNav-B86GsKfM.js";import{_ as c,o as _,c as r,g as n,a as p,F as i}from"./index-B3CzXvhw.js";const m={name:"LoginView",components:e,Socials:s};function l(u,o,g,d,f,V){const a=e,t=s;return _(),r(i,null,[n(a),n(t),o[0]||(o[0]=p("div",{class:"page"},"Login",-1))],64)}const B=c(m,[["render",l]]);export{B as default};
