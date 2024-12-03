20073b5f41fa9f7d4a9cf2dd6ccca7a514faa84c
luckybluejay27 <luckybluejay27@gmail.com>
2024-12-03 03:07:08 -0800

    Fixing Persistent Elements with CSS Refactor

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
+  <!-- Navbar -->
+  <div class="TopPanel">
+    <MainNav />
+    <Socials />
+  </div>
+  <!-- Content -->
   <div id="app" class="container">
     <router-view />
   </div>
+  <!-- Footer -->
   <h1 class="break-line" />
   <BottomPanel />
 </template>
 
 <script>
   import BottomPanel from './components/BottomPanel.vue'
+  import MainNav from './components/MainNav.vue'
+  import Socials from './components/Socials.vue'
   import './styles/GlobalDark.scss'
 
   export default {
     name: 'App',
     components: {
       BottomPanel,
+      MainNav,
+      Socials,
     },
   }
 </script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,38 +1,9 @@
 <template>
-  <div class="top-bar">
-    <MainNav />
-    <Socials />
-  </div>
-  <div class="page">
-    <!-- Shark Splash -->
-    <div class="SharkSplash">
-      <img src="../assets/SharkSplashTitle.svg">
-      <h1 class="index-title">Animation: Untethered</h1>
-    </div>
-    <!-- Carousel -->
-    <CarouselPromo />
-    <!-- Our Mission -->
-    <div class="OurMission">
-      <h1>Our Mission</h1>
-      <p>
-        To empower creation without external pressure, fostering an environment
-        free from convention. Through collaboration, we support each other.
-      </p>
-    </div>
-  </div>
+  <div class="break-line" />
+  <br>
+  <br>
+  <p>Empty Params</p>
 </template>
 
 <script>
-  import MainNav from '@/components/MainNav.vue'
-  import CarouselPromo from '../components/CarouselPromo.vue'
-  import Socials from '../components/Socials.vue'
-
-  export default {
-    name: 'Home',
-    components: {
-      MainNav,
-      CarouselPromo,
-      Socials,
-    },
-  }
 </script>
