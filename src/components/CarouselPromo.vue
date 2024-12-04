ae5d910183b5567156a70fd530c26dc2e5d4a536
luckybluejay27 <luckybluejay27@gmail.com>
2024-12-04 04:39:28 -0800

    Fixed MainNav and TopPanel

--- src/components/CarouselPromo.vue
+++ src/components/CarouselPromo.vue
@@ -1,9 +1,26 @@
+<template>
+  <div class="CarouselPromo">
+    <h1>Our Works</h1>
+    <div class="carousel-container">
+      <div class="carousel-wrapper">
+        <div v-for="project in projects" :key="project.id" class="carousel-item">
+          <img :alt="project.name" class="project-image" :src="project.image" :status="project.status">
+          <div v-if="project.status === 'in-progress'" class="status-overlay">
+            IN PROGRESS
+          </div>
+          <p>{{ project.name }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script>
   import CarcinogenImg from '@/assets/Carcinogen.jpg'
   import CoffeeAndMagicImg from '@/assets/Coffee & Magic.jpg'
 
   export default {
-    name: 'Carousel',
+    name: 'CarouselPromo',
     data () {
       return {
         projects: [
@@ -24,20 +41,3 @@
     },
   }
 </script>
-
-<template>
-  <div class="CarouselPromo">
-    <h1>Our Works</h1>
-    <div v-for="project in projects" :key="project.id" class="carousel-item">
-      <div class="image-container" :status="project.status">
-        <img :alt="project.name" :src="project.image">
-        <div v-if="project.status === 'in-progress'" class="status-overlay">
-          IN PROGRESS
-        </div>
-      </div>
-      <p>{{ project.name }}</p>
-    </div>
-  </div>
-</template>
-
-<style lang="scss"></style>
